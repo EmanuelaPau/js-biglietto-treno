6542ce8dd8f17a986013f8b0eea83d84b6d5ff26
EmanuelaPau <uapame@gmail.com>
2023-05-12 17:47:24 +0200

    Made JS code more simple

--- script/my_script.js
+++ script/my_script.js
@@ -1,32 +1,20 @@
 // Ask length of the trip
 const kilometers = parseInt(prompt("quanti chilometri devi percorrere?"));
 
-// Lenght of the trip written in numbers verification
-console.log(typeof kilometers)
-if (isNaN(kilometers)) {
-    console.log("C'è un errore nella compilazione dei chilometri da percorrere")
-    document.getElementById("alert").innerHTML = `È stato rilevato un errore nel campo: chilometri da percorrere. Per favore ricaricare la pagina e riscrivere il numero di chilometri in forma numerica`;
-
-    throw new Error("Age was badly written!");
-}
-
-
 // Ask passenger Age
 const passengerAge = parseInt(prompt("quanti anni hai?"));
-console.log(passengerAge);
 
-// Age written in numbers verification
+// Lenght of the trip written in numbers &Age written in numbers verification
 console.log(typeof passengerAge)
-if (isNaN(passengerAge)) {
-    console.log("C'è un errore nella compilazione dell'età")
-    document.getElementById("alert").innerHTML = `È stato rilevato un errore nel campo: età del passeggero. Per favore ricaricare la pagina e riscrivere l'età in forma numerica`;
+if (isNaN(passengerAge) || (isNaN(kilometers))) {
+    console.log("C'è un errore nella compilazione")
+    document.getElementById("alert").innerHTML = `È stato rilevato un errore. Per favore ricaricare la pagina e riscrivere in forma numerica`;
 
     throw new Error("Age was badly written!");
 }
 
-
 // Calc ticket price: 0.21 € al km)
-const fullTicketPrice = kilometers * 0.21;
+const fullTicketPrice = (kilometers * 0.21).toFixed(2);
 console.log(`Your not discounted ticket price is: ${fullTicketPrice} Euros`);
 
 
@@ -35,30 +23,18 @@ let discountedticketPrice;
 
 if (passengerAge < 18) {
     // allora applica sconto del 20 % sul prezzo totale 
-    discountedticketPrice = fullTicketPrice - (fullTicketPrice * 0.2);
+    discountedticketPrice = (fullTicketPrice - (fullTicketPrice * 0.2)).toFixed(2);
 
     // Print result to page 
-    document.getElementById("your-ticket-price").innerHTML = `Il prezzo intero del tuo biglietto è: ${fullTicketPrice}€.`;
-
     document.getElementById("your-discounted-ticket-price").innerHTML = `Hai diritto allo sconto giovani del 20%, per cui il prezzo finale del tuo biglietto è: ${discountedticketPrice}€`;
 
 } else if (passengerAge > 65) {
     // allora applica sconto del 40 % sul prezzo totale 
-    discountedticketPrice = fullTicketPrice - (fullTicketPrice * 0.4);
+    discountedticketPrice = (fullTicketPrice - (fullTicketPrice * 0.4)).toFixed(2);
     // Print result to page 
-    document.getElementById("your-ticket-price").innerHTML = `Il prezzo intero del suo biglietto è: ${fullTicketPrice}€.`;
-
     document.getElementById("your-discounted-ticket-price").innerHTML = `Ha diritto allo sconto anziani del 40%, per cui il prezzo finale del suo biglietto è: ${discountedticketPrice}€`;
-} else {
-    // Print result to page 
-    document.getElementById("your-ticket-price").innerHTML = `Il prezzo del tuo biglietto è: ${fullTicketPrice}€`;
-
 }
 
 // Fix result with two decimals
-discountedticketPrice = discountedticketPrice.toFixed(2);
 
-console.log(`Your discounted ticket price is: ${discountedticketPrice} Euros`);
-
-// // Print result to page 
-// document.getElementById("your-ticket-price").innerHTML = `Il prezzo del tuo biglietto è: ${discountedticketPrice}€`;
+document.getElementById("your-ticket-price").innerHTML = `${fullTicketPrice}€.`;
