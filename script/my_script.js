7afacf0fc74e42c9b0d4b0a237039f1c7c8ae05a
EmanuelaPau <uapame@gmail.com>
2023-05-12 15:18:29 +0200

    Add if else equation

--- script/my_script.js
+++ script/my_script.js
@@ -2,16 +2,36 @@
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana in pagina (con massimo due decimali, per indicare centesimi sul prezzo).
 // Questo richiederà un minimo di ricerca.
 
+// Ask length of the trip
 const kilometers = parseInt(prompt("quanti chilometri devi percorrere?"));
 console.log(kilometers);
 
+// Ask passenger Age
 const passengerAge = parseInt(prompt("quanti anni hai?"));
 console.log(passengerAge);
 
+// Calc ticket price: 0.21 € al km)
 const fullTicketPrice = kilometers * 0.21;
-console.log(`Your not discounted ticket price is: ${fullTicketPrice}` Euros);
+console.log(`Your not discounted ticket price is: ${fullTicketPrice} Euros`);
+
+// Add discounted ticket variable
+let discountedticketPrice;
+
+if (passengerAge < 18) {
+    let discountedticketPrice = fullTicketPrice - (fullTicketPrice * 0.2);
+    // allora applica sconto del 20 % sul prezzo totale 
+} else if (passengerAge > 65) {
+    let discountedticketPrice = fullTicketPrice - (fullTicketPrice * 0.4);
+    // allora applica sconto del 40 % sul prezzo totale 
+} else {
+    let discountedticketPrice = fullTicketPrice;
+    // il prezzo rimane quello calcolato
+}
+
+console.log(`Your discounted ticket price is: ${discountedticketPrice} Euros`);
